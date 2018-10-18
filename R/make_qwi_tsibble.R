197bf5f05cf81f5106e5ba5a4c31f4d97130ce8d
medewitt <me.dewitt.jr@gmail.com>
2018-10-18 06:45:37 -0400

    initial commit

---
+++ R/make_qwi_tsibble.R
@@ -0,0 +1,12 @@
+#' Convert to A Tibble
+#' @param df The dataframe returns from the get_qwi function
+#' @return a tsibble object with the correct nexting
+#'
+#'
+#' @export
+#' @import tsibble
+#'
+
+make_qwi_tsibble <- function(df){
+
+}

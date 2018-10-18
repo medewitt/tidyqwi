306b75e603c7eecfbef74f2cc97a5a2e87a81b69
medewitt <me.dewitt.jr@gmail.com>
2018-10-18 12:22:12 -0400

    some more attempts. really rought. tsibble not fully developed to support rlang or SE

--- R/make_qwi_tsibble.R
+++ R/make_qwi_tsibble.R
@@ -7,6 +7,51 @@
 #' @import tsibble
 #'
 
+dat <- get_qwi(years = "2010", states = "37", apikey = census_key, geography = "county")
+names(dat)
 make_qwi_tsibble <- function(df){
+  #Need to Determine Nesting Structure Based on Columns if nesting is not specified
+  col_names <- names(df)
+
+  if(!"state" %in% col_names){
+    stop("This is not the standard qwi data frame. Please specify keys.")
+  }
+
+  if("metropolitan statistical area/micropolitan statistical area" %in% col_names){
+    message("Using MSA")
+    #id_var <- "msa | state,  agegrp, ownercode, sex, industry"
+    as_tsibble(df, key = id(`metropolitan statistical area/micropolitan statistical area`| state, agegrp, ownercode, sex, industry),
+               index = year_time)
+  } else{
+    message("Using County")
+    #id_var <- "county | state state, agegrp, ownercode, sex, industry"
+
+    as_tsibble(df, key = id(county| state, agegrp, ownercode, sex, industry),
+               index = year_time)
+
+  }
+  #nesting_structure <<- rlang::enquo(id_var)
 
 }
+nesting_structure
+test <- make_qwi_tsibble(dat)
+test
+id_var
+class(test)
+library(rlang)
+nesting_structure
+library(hts)
+library(forecast)
+fcasts2.mo <- forecast(
+  test, h = 5, fmethod = "ets", level = 1,
+  keep.fitted = TRUE, keep.resid = TRUE
+)
+test
+summary(test)
+dat->a#%>%
+dat
+as_tsibble(validate = dat,
+           key = id(`metropolitan statistical area/micropolitan statistical area` | state),
+           index = index(year_time))
+
+as_tsibble(dat, key = id(county| state, agegrp, ownercode, sex, industry), index = year_time)

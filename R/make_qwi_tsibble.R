6cd854c5a8c7c1760d0108e3b3fc4d1a3dfdf930
Michael DeWitt <me.dewitt.jr@gmail.com>
2018-11-23 13:48:16 -0500

    clean up old stuff

--- R/make_qwi_tsibble.R
+++ R/make_qwi_tsibble.R
@@ -7,8 +7,6 @@
 #' @import tsibble
 #'
 
-dat <- get_qwi(years = "2010", states = "37", apikey = census_key, geography = "county")
-names(dat)
 make_qwi_tsibble <- function(df){
   #Need to Determine Nesting Structure Based on Columns if nesting is not specified
   col_names <- names(df)
@@ -33,25 +31,3 @@ make_qwi_tsibble <- function(df){
   #nesting_structure <<- rlang::enquo(id_var)
 
 }
-nesting_structure
-test <- make_qwi_tsibble(dat)
-test
-id_var
-class(test)
-library(rlang)
-nesting_structure
-library(hts)
-library(forecast)
-fcasts2.mo <- forecast(
-  test, h = 5, fmethod = "ets", level = 1,
-  keep.fitted = TRUE, keep.resid = TRUE
-)
-test
-summary(test)
-dat->a#%>%
-dat
-as_tsibble(validate = dat,
-           key = id(`metropolitan statistical area/micropolitan statistical area` | state),
-           index = index(year_time))
-
-as_tsibble(dat, key = id(county| state, agegrp, ownercode, sex, industry), index = year_time)

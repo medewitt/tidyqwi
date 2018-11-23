7df627f86c631c7d4d9c07db9d0bc17657ed647f
Michael DeWitt <me.dewitt.jr@gmail.com>
2018-11-23 13:53:18 -0500

    make mo betta

--- R/make_qwi_tsibble.R
+++ R/make_qwi_tsibble.R
@@ -1,11 +1,8 @@
 #' Convert to A Tibble
 #' @param df The dataframe returns from the get_qwi function
 #' @return a tsibble object with the correct nexting
-#'
-#'
-#' @export
 #' @import tsibble
-#'
+#' @export
 
 make_qwi_tsibble <- function(df){
   #Need to Determine Nesting Structure Based on Columns if nesting is not specified
@@ -18,13 +15,13 @@ make_qwi_tsibble <- function(df){
   if("metropolitan statistical area/micropolitan statistical area" %in% col_names){
     message("Using MSA")
     #id_var <- "msa | state,  agegrp, ownercode, sex, industry"
-    as_tsibble(df, key = id(`metropolitan statistical area/micropolitan statistical area`| state, agegrp, ownercode, sex, industry),
+    tsibble::as_tsibble(df, key = id(`metropolitan statistical area/micropolitan statistical area`| state, agegrp, ownercode, sex, industry),
                index = year_time)
   } else{
     message("Using County")
     #id_var <- "county | state state, agegrp, ownercode, sex, industry"
 
-    as_tsibble(df, key = id(county| state, agegrp, ownercode, sex, industry),
+    tsibble::as_tsibble(df, key = id(county| state, agegrp, ownercode, sex, industry),
                index = year_time)
 
   }
